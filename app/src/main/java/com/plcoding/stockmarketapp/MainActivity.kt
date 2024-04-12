3eed08e48d62d5a8609316e49fe1ff6ebe2ddb0e
Omar Issa <oiattar1@gmail.com>
2024-04-12 15:54:13 -0700

    fix gradle

--- app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.plcoding.stockmarketapp.presentation.company_listings.NavGraphs
 import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    //DestinationsNavHost(navGraph = NavGraphs.root)
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
